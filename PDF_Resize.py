025654cfb66b292a0ffa0d8af92ff8d1faada8c6
Jonas T. <jonas.toens@kabelsurfer.de>
2025-06-23 13:57:33 +0200

    added basic ui elements

--- PDF_Resize.py
+++ PDF_Resize.py
@@ -1,10 +1,55 @@
 import sys
 from pypdf import PdfReader, PdfWriter
+from PySide6 import QtCore, QtWidgets
 
 
-def main():
-    file = sys.argv[1]
-    reader = PdfReader(file)
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.inputPath = ""
+
+        self.selectButton = QtWidgets.QPushButton("Datei...")
+        self.saveButton = QtWidgets.QPushButton("Speichern unter...")
+        self.fileName = QtWidgets.QLabel("", alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.selectButton)
+        self.layout.addWidget(self.saveButton)
+        self.layout.addWidget(self.fileName)
+
+        self.selectButton.clicked.connect(self.magic)
+        self.saveButton.clicked.connect(self.saveUnder)
+
+    @QtCore.Slot()
+    def magic(self):
+        fileName = QtWidgets.QFileDialog.getOpenFileName(
+            self, "Open File", "", self.tr("PDF Files (*.pdf)")
+        )
+        filePath = fileName[0]
+        self.inputPath = filePath
+        self.fileName.setText(filePath)
+
+    @QtCore.Slot()
+    def saveUnder(self):
+        fileName, _ = QtWidgets.QFileDialog.getSaveFileName(
+            self, "Save File", "", self.tr("PDF Files (*.pdf)")
+        )
+
+        if len(fileName) < 4:
+            return
+
+        # Check for file type existance
+        if fileName[-4:] != ".pdf":
+            fileName += ".pdf"
+
+        if self.inputPath == "":
+            return
+
+        resizePDF(self.inputPath, fileName)
+
+
+def resizePDF(inputFile, outputFile):
+    reader = PdfReader(inputFile)
     writer = PdfWriter()
 
     page = reader.pages[0]
@@ -17,8 +62,19 @@ def main():
 
     writer.add_page(page)
 
-    with open("output.pdf", "wb") as f:
+    with open(outputFile, "wb") as f:
         writer.write(f)
+
+
+def main():
+    # file = sys.argv[1]
+    # resizePDF(file)
+    app = QtWidgets.QApplication([])
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
