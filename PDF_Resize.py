a97696e5d27b137a262340c5ba90c646b4134e9d
Jonas T. <jonas.toens@kabelsurfer.de>
2025-06-24 15:42:28 +0200

    added save under and print

--- PDF_Resize.py
+++ PDF_Resize.py
@@ -1,27 +1,34 @@
+import os
+import subprocess
 import sys
 from pypdf import PdfReader, PdfWriter
 from PySide6 import QtCore, QtWidgets
+from PySide6.QtPrintSupport import QPrinter, QPrintPreviewDialog, QPrintDialog
 
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.inputPath = ""
+        self.outputPath = ""
 
         self.selectButton = QtWidgets.QPushButton("Datei...")
         self.saveButton = QtWidgets.QPushButton("Speichern unter...")
+        self.printButton = QtWidgets.QPushButton("Drucken")
         self.fileName = QtWidgets.QLabel("", alignment=QtCore.Qt.AlignCenter)
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.selectButton)
         self.layout.addWidget(self.saveButton)
+        self.layout.addWidget(self.printButton)
         self.layout.addWidget(self.fileName)
 
-        self.selectButton.clicked.connect(self.magic)
+        self.selectButton.clicked.connect(self.selectInputFile)
         self.saveButton.clicked.connect(self.saveUnder)
+        self.printButton.clicked.connect(self.print)
 
     @QtCore.Slot()
-    def magic(self):
+    def selectInputFile(self):
         fileName = QtWidgets.QFileDialog.getOpenFileName(
             self, "Open File", "", self.tr("PDF Files (*.pdf)")
         )
@@ -29,23 +36,40 @@ class MyWidget(QtWidgets.QWidget):
         self.inputPath = filePath
         self.fileName.setText(filePath)
 
-    @QtCore.Slot()
-    def saveUnder(self):
-        fileName, _ = QtWidgets.QFileDialog.getSaveFileName(
+    def selectOutputPath(self):
+        outputPath, _ = QtWidgets.QFileDialog.getSaveFileName(
             self, "Save File", "", self.tr("PDF Files (*.pdf)")
         )
 
-        if len(fileName) < 4:
+        if len(outputPath) < 4:
+            print("Path is too short")
             return
 
         # Check for file type existance
-        if fileName[-4:] != ".pdf":
-            fileName += ".pdf"
+        if outputPath[-4:] != ".pdf":
+            outputPath += ".pdf"
+
+        return outputPath
+
+    @QtCore.Slot()
+    def saveUnder(self):
+
+        self.outputPath = self.selectOutputPath()
 
         if self.inputPath == "":
+            print("No file selected")
             return
 
-        resizePDF(self.inputPath, fileName)
+        resizePDF(self.inputPath, self.outputPath)
+
+    def print(self):
+
+        if sys.platform == "win32":
+            os.startfile(self.outputPath)
+        elif sys.platform == "darwin":
+            subprocess.run(["open", "-a", "Preview", self.outputPath])
+        else:
+            subprocess.run(["lpr", self.outputPath])
 
 
 def resizePDF(inputFile, outputFile):
